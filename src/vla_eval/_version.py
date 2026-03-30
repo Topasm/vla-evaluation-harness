fb6d4b8e117ceae416e29deaf3da20e737d4d862
Topasm <dhkdwk0215@naver.com>
2026-03-30 13:58:37 +0900

    feat: add configuration files for LIBERO-10 quick test and SemTok-VLA Long suite fix: update version to 0.0.4.dev8+g1b1113213.d20260328

--- src/vla_eval/_version.py
+++ src/vla_eval/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev1+g18749fd21'
-__version_tuple__ = version_tuple = (0, 1, 'dev1', 'g18749fd21')
+__version__ = version = '0.0.4.dev8+g1b1113213.d20260328'
+__version_tuple__ = version_tuple = (0, 0, 4, 'dev8', 'g1b1113213.d20260328')
 
 __commit_id__ = commit_id = None
